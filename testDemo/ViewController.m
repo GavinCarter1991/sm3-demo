e888b0bb6e65f6146f58a4d273fd35df34f57642
gavin <kindyourself@163.com>
2025-06-13 10:50:56 +0800

    delect SM3Encryptor

--- testDemo/ViewController.m
+++ testDemo/ViewController.m
@@ -6,8 +6,6 @@
 //
 
 #import "ViewController.h"
-#import "SM3Encryptor.h"
-//#import "GMObjC.h"
 #import "GMObjC/GMObjC.h"
 
 @interface ViewController ()
@@ -19,12 +17,6 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     NSString *str = @"Abc@1234";
-//    NSString *pw = [SM3Encryptor sm3HashWithString:str];
-//    NSLog(@"%@", pw);
-//    
-//    NSString *digest = [GMSm3Utils hashWithText:str];
-//    NSLog(@"%@", digest);
-    
     NSString *digest1 = [GMSm3Utils hashWithText:str];
     NSLog(@"%@", digest1);
 }
