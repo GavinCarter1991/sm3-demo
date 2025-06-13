23fb033b0f92c86365565dc006abd71f0406cdf8
gavin <kindyourself@163.com>
2025-06-13 10:36:43 +0800

    commit

---
+++ testDemo/ViewController.m
@@ -0,0 +1,33 @@
+//
+//  ViewController.m
+//  testDemo
+//
+//  Created by wt on 2025/6/12.
+//
+
+#import "ViewController.h"
+#import "SM3Encryptor.h"
+#import "GMObjC.h"
+//#import "GMObjC/GMObjC.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSString *str = @"Abc@1234";
+//    NSString *pw = [SM3Encryptor sm3HashWithString:str];
+//    NSLog(@"%@", pw);
+//    
+    NSString *digest = [GMSm3Utils hashWithText:str];
+    NSLog(@"%@", digest);
+    
+//    NSString *digest1 = [GMSm3Utils hashWithText:str];
+//    NSLog(@"%@", digest1);
+}
+
+
+@end
